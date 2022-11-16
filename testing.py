91bb48bc2e2d4d8e20c7bfe3c540becefc9347e2
tim <tmktcq@umsystem.edu>
2022-11-16 09:43:19 -0600

    SHL function test

--- testing.py
+++ testing.py
@@ -1,3 +1,13 @@
 ''' Created this for testing functions individually '''
 
+A = "10100011"
 
+def shl(dividend):
+    dividend +="_"
+    E = dividend[0]
+    A = dividend[1:int((len(dividend)-1)/2)] #should be from [1:]
+    Q = dividend[int(len(dividend)/2): len(dividend)]
+
+    return E, A , Q 
+print(A)
+print(shl(A))
