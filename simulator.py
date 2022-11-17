11b7fcdf351ad4727397059a820d62bd13dda18c
tim <tmktcq@umsystem.edu>
2022-11-17 15:14:10 -0600

    n

--- simulator.py
+++ simulator.py
@@ -20,29 +20,60 @@ def main():
         (9  ≤  dividend  length  ≤  25  and  5  ≤  divisor  length  ≤ 13)
     '''
 
-def restoring(dividend, divisor): #put in print statements later
+def restoring(dividend, divisor): #Put in Print Statments later. 
 
-    i = len(divisor) # this is equal to the amout of shifts we will  have
-    B = '0' + divisor
+    i = len(divisor)-1 # this is equal to the amout of shifts we will  have
     NotB = onesComplement(B)
     while( i > 0 ):
-        A,Q= shl(dividend)
+        if(i != len(divisor)-1):
+            dividend = A + Q
+        E,A,Q= shl(dividend)
+        A = E+A
+        print("shift: "+A,Q  )
         SavedA = A
         A = add(A,NotB)
-        if(A[0] == 0):
-            Q-= '_'
-            Q+= '1'
+        print("Add: " + A)
+        if(A[0] == '0'):
+            print("A>B no restore")
+            Q = Q.replace('_','1')
+            
         else:
             A = SavedA
-            Q-= '_'
-            Q+= '0'
+            Q = Q.replace('_','0')
+            print("A<B restore")
+        i -= 1
 
-        i += 1
+    return A[1:len(A)] , Q 
+
+def nonrestoring(dividend, divisor): 
+    i = len(divisor)-1 # this is equal to the amout of shifts we will  have
+    NotB = onesComplement(B)
+    while( i > 0 ):
+        if(i != len(divisor)-1):
+            dividend = A + Q
+        print("Start of loop "+dividend)
+    
+        if(dividend[0] == "1"):
+            E,A,Q= shl(dividend)
+            print("Shift: " + E,A,Q)
+            A = E+A
+            A = add(A,B)
+            print("Add: " + A)
+        else:
+            E,A,Q= shl(dividend)
+            print("Shift: " + E,A,Q)
+            A = E+A
+            A = add(A,NotB)
+            print("Subtract: " + A)
+        if(A[0] == '0'):
+            Q = Q.replace('_','1')
+        else:
+            Q = Q.replace('_','0')
+        i-=1
+    if(A[0] == "1"):
+        A = add(A,B)
+    return A[1:len(A)] , Q 
         
-
-def nonrestoring(): 
-    placeholder = "empty"    
-
 def bin2dec(binary):
     decimal = 0
     for digit in binary:
@@ -50,12 +81,12 @@ def bin2dec(binary):
     return decimal
 
 def shl(dividend):
-    
-    A = dividend[0:(math.ceil((len(dividend)/2))+1)] #should be from [1:]
-    Q = dividend[int(len(dividend)/2)+1: len(dividend)]
+    E = dividend[1]
+    A = dividend[2:(math.ceil((len(dividend)/2))+1)] #should be from [1:]
+    Q = dividend[(math.ceil(len(dividend)/2)+1): len(dividend)]
     Q += "_"
     
-    return A , Q 
+    return E, A , Q 
     
 def sub(D,B): # Fix this
     '''Add in Inverse here '''
@@ -85,7 +116,6 @@ def add(num1,num2):
 
     result = ''
     carry = 0
-
     while(i >= 0):
         sum = int(num1[i]) + int(num2[i])
         if sum == 2: #1 + 1
@@ -115,4 +145,8 @@ def add(num1,num2):
 
 #shift using slice(1,len(string)) add in what E is with string+= 0 or 1
 
-main() 
+dividend = "111100001" # has to be EAQ 
+B = "01111"
+print(restoring(dividend, B))
+print()
+print(nonrestoring(dividend,B))
