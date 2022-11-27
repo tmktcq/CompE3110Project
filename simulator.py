97d341a300d49590a0ce02bbc9d5736dc01bde71
Kellermann <tmktcq@rc11xcs213.managed.mst.edu>
2022-11-27 12:04:09 -0600

    restoring almost finished

--- simulator.py
+++ simulator.py
@@ -1,5 +1,9 @@
 import math
 
+def sign(Qs, Ds):
+    sign = int(Qs[0]) ^ int(Ds[0])
+    sign = str(sign)
+    return sign 
 
 def main(): 
     D = "101"
@@ -23,8 +27,7 @@ def main():
 def restoring(dividend, divisor): #Put in Print Statments later. 
     sign = int(dividend[0]) ^ int(divisor[0])
     sign = str(sign)
-    dividend = "0" + dividend[1:]
-    divisor = '0' + divisor[1:]
+    divisor = "0" + divisor[1::]
     i = len(divisor)-1 # this is equal to the amout of shifts we will  have
     NotB = onesComplement(divisor) #does 1 and twos comp 
     print("  " + dividend + " = Dividend  " + divisor + " = divisor" )
@@ -49,7 +52,7 @@ def restoring(dividend, divisor): #Put in Print Statments later.
             print("E=1, A<0 restore Q0=0")
         i -= 1
 
-    return A[1:len(A)] , (sign+Q)  # adds sign into the Quotient reminder is always > 0
+    return A[1:len(A)] , (sign+Q)  # adds sign into the Quotient 
 
 def nonrestoring(dividend, divisor): 
     i = len(divisor)-1 # this is equal to the amout of shifts we will  have
@@ -97,7 +100,7 @@ def shl(dividend):
 def sub(D,B): # Fix this
     '''Add in Inverse here '''
     #Put adding after getting 2's complemnt. 
-    
+    return 0 
 
 def onesComplement(num):
     oneComp = 0
@@ -151,8 +154,8 @@ def add(num1,num2):
 
 #shift using slice(1,len(string)) add in what E is with string += 0 or 1
 
-dividend = "1111111000001" # has to be EAQ 
-B = "1111111"
+dividend = "1000011100001000100100000" # has to be EAQ 
+B = "0000011100000"
 
 print(restoring(dividend, B))
 print()
