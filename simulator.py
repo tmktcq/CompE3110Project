821c4d8f6507f4563519d39a98519ff39cac9d58
tim <tmktcq@umsystem.edu>
2022-11-28 08:28:26 -0600

    nonrestoring work

--- simulator.py
+++ simulator.py
@@ -1,11 +1,15 @@
 import math
 from prettytable import PrettyTable # pip install prettytable to use library 
 
+
 restoringTable = PrettyTable()
 nonrestoringTable = PrettyTable()
+metricsTable = PrettyTable()
 
 restoringTable.field_names = ["E", "A" , "Q" , "Notes"]
 nonrestoringTable.field_names = ["E", "A" , "Q" , "Notes"]
+metricsTable.field_names = ["Length of Dividend", "Length of Divisor" ,"Iterations", "Non-Restoring Add/Sub Count" , "Restoring Add/Sub Count"]
+
 
 def main(): 
     D = "101"
@@ -27,6 +31,7 @@ def main():
     '''
 
 def restoring(dividend, divisor): #Put in Print Statments later. 
+    restAddSubCount = 0
     sign = int(dividend[0]) ^ int(divisor[0])
     sign = str(sign)
     divisor = "0" + divisor[1::]
@@ -41,7 +46,7 @@ def restoring(dividend, divisor): #Put in Print Statments later.
         SavedA = A
         A = add(A,NotB)
         restoringTable.add_row([NotB[0],NotB[1::],"", "Add ~B"])
-
+        restAddSubCount = restAddSubCount + 1
         if(A[0] == '0'):
             Q = Q.replace('_','1')   
             restoringTable.add_row([A[0],A[1::],Q, "E=0, A>B no restore Q0=1"])
@@ -49,13 +54,21 @@ def restoring(dividend, divisor): #Put in Print Statments later.
             A = SavedA
             Q = Q.replace('_','0')
             restoringTable.add_row([A[0],A[1::],Q, "E=1, A<0 restore Q0=0"])
+            restAddSubCount +=1
         i -= 1
     restoringTable.add_row(["",A[1::],(sign+Q), "Reminder and Quotient"])
     restoringTable.add_row(["", binToHexa((A[1::])), binToHexa(sign+Q), "Results in Hexidecimal" ])
+    return restAddSubCount
+
 
 def nonrestoring(dividend, divisor): 
+    nonrestAddSubCount = 0
+    sign = int(dividend[0]) ^ int(divisor[0])
+    sign = str(sign)
+    divisor = "0" + divisor[1::]
+    dividend = "0" + dividend[1::]
     i = len(divisor)-1 # this is equal to the amout of shifts we will  have
-    NotB = onesComplement(B)
+    NotB = onesComplement(divisor)
     E = "0"
     while( i > 0):
         if(i != len(divisor)-1):
@@ -63,24 +76,33 @@ def nonrestoring(dividend, divisor):
     
         if(dividend[0] == "1"):
             E,A,Q= shl(dividend)
-            print("Shift: " + E,A,Q)
+            nonrestoringTable.add_row([E,A,Q, "SHL"])
             A = E+A
-            A = add(A,B)
-            print("Add: " + A)
+            A = add(A,divisor)
+            nonrestAddSubCount +=1
+            nonrestoringTable.add_row([divisor[0],divisor[1::],"", "Add B"])
         else:
             E,A,Q= shl(dividend)
-            print("Shift: " + E,A,Q)
+            nonrestoringTable.add_row([E,A,Q, "SHL"])
             A = E+A
             A = add(A,NotB)
-            print("Subtract: " + A)
+            nonrestAddSubCount +=1
+            nonrestoringTable.add_row([NotB[0],NotB[1::],"", "Add ~B"])
         if(A[0] == '0'):
             Q = Q.replace('_','1')
+            nonrestoringTable.add_row([A[0],A[1::],Q, "E=0, Q0=1"])
         else:
             Q = Q.replace('_','0')
+            nonrestoringTable.add_row([A[0],A[1::],Q, "E=1, Q0=0"])
         i-=1
     if(A[0] == "1"):
-        A = add(A,B)
-    return A[0], A[1:len(A)] , Q 
+        nonrestoringTable.add_row([divisor[0],divisor[1::],"", "Add B"])
+        nonrestAddSubCount +=1
+        A = add(A,divisor)
+
+    nonrestoringTable.add_row(["",A[1::],(sign+Q), "Reminder and Quotient"])
+    nonrestoringTable.add_row(["", binToHexa((A[1::])), binToHexa(sign+Q), "Results in Hexidecimal" ])
+    return nonrestAddSubCount
         
 def bin2dec(binary):
     decimal = 0
@@ -208,11 +230,33 @@ def add(num1,num2):
     result = result[::-1]
     return result
 
-dividend = "001001101" # has to be EAQ 
-B = "11011"
+nonRestAddSubCount = 0 
+dividend = "0010110110010" # has to be EAQ 
+B = "1011011"
+#metricsTable.field_names = ["Length of Dividend", "Length of Divisor" ,"Iterations", "Non-Restoring Add/Sub Count" , "Restoring Add/Sub Count"]
 
-restoring(dividend, B)
-print()
+
+print("\n\n\n")
+
+restAddSubCount = restoring(dividend, B)
+print("+----------------------------------------+")
+print("|                                        |")
+print("|          Restoring Algorithm           |")
+print("|                                        |")
+print("+----------------------------------------+")
+
 print(restoringTable)
+
+print("\n\n")
+
+nonRestAddSubCount = nonrestoring(dividend,B)
+print("+----------------------------------------+")
+print("|                                        |")
+print("|        Non-Restoring Algorithm         |")
+print("|                                        |")
+print("+----------------------------------------+")
+print(nonrestoringTable)
+
 print()
-print(nonrestoring(dividend,B))
+metricsTable.add_row([len(dividend), len(B), len(B)-1, nonRestAddSubCount, restAddSubCount])
+print(metricsTable)
