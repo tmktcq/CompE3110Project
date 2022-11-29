d33c6c74b6a79a233a6eb9dc67b9e175b130bcc3
tim <tmktcq@umsystem.edu>
2022-11-29 08:41:30 -0600

    done??

--- simulator.py
+++ simulator.py
@@ -1,6 +1,20 @@
+''' 
+    Names: Tim Kellermann, Sudeep Potluri
+    Project: Restoring vs. Nonrestoring division Algorithm
+    Due Date: 11/29/2022
+    file name: simulator.py 
+'''
+
 import math
 from prettytable import PrettyTable # pip install prettytable to use library 
 
+'''
+   +---------------------------------------------------+
+   |                                                   |
+   |                 Global Variables                  |
+   |                                                   |
+   +---------------------------------------------------+
+'''
 
 restoringTable = PrettyTable()
 nonrestoringTable = PrettyTable()
@@ -10,13 +24,28 @@ restoringTable.field_names = ["E", "A" , "Q" , "Notes"]
 nonrestoringTable.field_names = ["E", "A" , "Q" , "Notes"]
 metricsTable.field_names = ["Length of Dividend", "Length of Divisor" ,"Iterations", "Non-Restoring Add/Sub Count" , "Restoring Add/Sub Count"]
 
+dividend = "010100011" # has to be EAQ 
+B = "01011"
 
-def restoring(dividend, divisor): #Put in Print Statments later. 
+'''
+   +---------------------------------------------------+
+   |                                                   |
+   |               Function Definitions                |
+   |                                                   |
+   +---------------------------------------------------+
+
+   Purpose: Executes the Restoring division algorithm and logs steps taken.
+   Pre-Conditions: input dividend as EAQ, and Divisor
+   Post-Conditions: returns the number of additions/subtractions 
+'''
+def restoring(dividend, divisor): 
+
     restAddSubCount = 0
-    sign = int(dividend[0]) ^ int(divisor[0])
+    sign = int(dividend[0]) ^ int(divisor[0]) #calculates sign bit
     sign = str(sign)
     divisor = "0" + divisor[1::]
 
+    #divide overflow check
     if(divideOverflow(dividend,divisor)):
         restoringTable.add_row([" Divide Overflow has occured"," First half of A>=B","", ""])
         return "Divide Overflow"
@@ -26,34 +55,39 @@ def restoring(dividend, divisor): #Put in Print Statments later.
     while( i > 0 ):
         if(i != len(divisor)-1):
             dividend = A + Q
-        E,A,Q= shl(dividend)
+        E,A,Q= shl(dividend) #Shift left 
         restoringTable.add_row([E,A,Q, "SHL"])
         A = E+A
         SavedA = A
         A = add(A,NotB)
-        restoringTable.add_row([NotB[0],NotB[1::],"", "Add ~B"])
-        restAddSubCount = restAddSubCount + 1
+        restoringTable.add_row([NotB[0],NotB[1::],"", "Add ~B"]) #this is used to log steps 
+        restAddSubCount = restAddSubCount + 1 #increments the addition counter
         if(A[0] == '0'):
             Q = Q.replace('_','1')   
-            restoringTable.add_row([A[0],A[1::],Q, "E=0, A>B no restore Q0=1"])
+            restoringTable.add_row([A[0],A[1::],Q, "E=0, A>B no restore Q0=1"]) #this is used to log steps 
         else:
             A = SavedA
             Q = Q.replace('_','0')
-            restoringTable.add_row([A[0],A[1::],Q, "E=1, A<0 restore Q0=0"])
-            restAddSubCount +=1
+            restoringTable.add_row([A[0],A[1::],Q, "E=1, A<0 restore Q0=0"]) #this is used to log steps 
+            restAddSubCount +=1 #increments the addition counter
         i -= 1
-    restoringTable.add_row(["",A[1::],(sign+Q), "Reminder and Quotient"])
-    restoringTable.add_row(["", binToHexa((A[1::])), binToHexa(sign+Q), "Results in Hexidecimal" ])
+    restoringTable.add_row(["",A[1::],(sign+Q), "Reminder and Quotient"]) #Solution in binary
+    restoringTable.add_row(["", binToHexa((A[1::])), binToHexa(sign+Q), "Results in Hexidecimal" ]) #Solution in hexadecimal
     return restAddSubCount
 
-
+''' 
+   Purpose: Executes the non-Restoring division algorithm and logs steps taken.
+   Pre-Conditions: input dividend as EAQ, and Divisor
+   Post-Conditions: returns the number of additions/subtractions 
+'''
 def nonrestoring(dividend, divisor): 
     nonrestAddSubCount = 0
-    sign = int(dividend[0]) ^ int(divisor[0])
+    sign = int(dividend[0]) ^ int(divisor[0]) #calculates sign of quotient
     sign = str(sign)
     divisor = "0" + divisor[1::]
     dividend = "0" + dividend[1::]
 
+    #divide overflow check
     if(divideOverflow(dividend,divisor)):
         nonrestoringTable.add_row([" Divide Overflow has occured"," First half of A>=B","", ""])
         return "Divide Overflow"
@@ -67,57 +101,77 @@ def nonrestoring(dividend, divisor):
     
         if(dividend[0] == "1"):
             E,A,Q= shl(dividend)
-            nonrestoringTable.add_row([E,A,Q, "SHL"])
+            nonrestoringTable.add_row([E,A,Q, "SHL"]) #this is used to log steps 
             A = E+A
             A = add(A,divisor)
-            nonrestAddSubCount +=1
-            nonrestoringTable.add_row([divisor[0],divisor[1::],"", "Add B"])
+            nonrestAddSubCount +=1 #increments the addition counter
+            nonrestoringTable.add_row([divisor[0],divisor[1::],"", "Add B"]) #this is used to log steps 
         else:
             E,A,Q= shl(dividend)
             nonrestoringTable.add_row([E,A,Q, "SHL"])
             A = E+A
             A = add(A,NotB)
-            nonrestAddSubCount +=1
-            nonrestoringTable.add_row([NotB[0],NotB[1::],"", "Add ~B"])
+            nonrestAddSubCount +=1 #increments the addition counter
+            nonrestoringTable.add_row([NotB[0],NotB[1::],"", "Add ~B"]) #this is used to log steps 
         if(A[0] == '0'):
             Q = Q.replace('_','1')
-            nonrestoringTable.add_row([A[0],A[1::],Q, "E=0, Q0=1"])
+            nonrestoringTable.add_row([A[0],A[1::],Q, "E=0, Q0=1"]) #this is used to log steps 
         else:
             Q = Q.replace('_','0')
-            nonrestoringTable.add_row([A[0],A[1::],Q, "E=1, Q0=0"])
+            nonrestoringTable.add_row([A[0],A[1::],Q, "E=1, Q0=0"]) #this is used to log steps 
         i-=1
     if(A[0] == "1"):
-        nonrestoringTable.add_row([divisor[0],divisor[1::],"", "Add B"])
-        nonrestAddSubCount +=1
+        nonrestoringTable.add_row([divisor[0],divisor[1::],"", "Add B"]) #this is used to log steps 
+        nonrestAddSubCount +=1 #increments the addition counter
         A = add(A,divisor)
 
-    nonrestoringTable.add_row(["",A[1::],(sign+Q), "Reminder and Quotient"])
-    nonrestoringTable.add_row(["", binToHexa((A[1::])), binToHexa(sign+Q), "Results in Hexidecimal" ])
+    nonrestoringTable.add_row(["",A[1::],(sign+Q), "Reminder and Quotient"]) #Result in Binary 
+    nonrestoringTable.add_row(["", binToHexa((A[1::])), binToHexa(sign+Q), "Results in Hexidecimal" ]) #Result in Hexadecimal 
     return nonrestAddSubCount
         
+''' 
+   Purpose: converts binary to decimal
+   Pre-Conditions: input a string that is binary
+   Post-Conditions: returns the decimal equivalent
+'''
 def bin2dec(binary):
     decimal = 0
     for digit in binary:
         decimal = decimal*2 + int(digit)
     return decimal
 
+''' 
+   Purpose: checks if divide overflow has occured
+   Pre-Conditions: input dividend and divisor
+   Post-Conditions: returns a bool if true then terminate program
+'''
 def divideOverflow(dividend, divisor):
-    A = dividend[1:(math.ceil((len(dividend)/2)))] #should be from [1:]
+    A = dividend[1:(math.ceil((len(dividend)/2)))]
     decDivisor = bin2dec((divisor[1::]))
     decA = bin2dec((A))
-    if(decA >= decDivisor):
+    if(decA >= decDivisor): # correlates to the partial Remainder greater than divisor 
         return True
     else:
         return False
 
+''' 
+   Purpose: shifts the binary number left 
+   Pre-Conditions: input binary string
+   Post-Conditions: returns EAQ from the shift
+'''
 def shl(dividend):
     E = dividend[1]
-    A = dividend[2:(math.ceil((len(dividend)/2))+1)] #should be from [1:]
-    Q = dividend[(math.ceil(len(dividend)/2)+1): len(dividend)]
-    Q += "_"
+    A = dividend[2:(math.ceil((len(dividend)/2))+1)] # takes the upper half of bits 
+    Q = dividend[(math.ceil(len(dividend)/2)+1): len(dividend)] # takes the lower half 
+    Q += "_" # adds a buffer as Q0
     
     return E, A , Q 
     
+''' 
+   Purpose: converts binary to hexadecimal
+   Pre-Conditions: input binary string
+   Post-Conditions: returns the hexadecimal equavalent 
+'''
 def binToHexa(n):
     bnum = int(n)
     temp = 0
@@ -172,14 +226,11 @@ def binToHexa(n):
         final = "0"
     return final    
 
-
-def sub(D,B): # Fix this
-    '''Add in Inverse here '''
-    #Put adding after getting 2's complemnt. 
-    return 0 
-
-    return 0 
-
+''' 
+   Purpose: calculates inverse 
+   Pre-Conditions: input binary string
+   Post-Conditions: returns the inverse as 2's comp
+'''
 def onesComplement(num):
     oneComp = 0
     oneCompStr = ""
@@ -187,17 +238,26 @@ def onesComplement(num):
         oneComp = 1^int(i)
         oneCompStr += str(oneComp)
 
-    oneCompStr = twosComplement(oneCompStr)
+    oneCompStr = twosComplement(oneCompStr) #adds 0x01 for twos comp 
     return oneCompStr
 
+''' 
+   Purpose: calculates 2's comp
+   Pre-Conditions: input binary string
+   Post-Conditions: returns the inverse as 2's comp
+'''
 def twosComplement(num):
     fill = len(num)
     one = "1"
-    one = one.zfill(fill)
+    one = one.zfill(fill) #fills the leading positions with 0's
     sum = (add(num,one)) 
     return sum
 
-
+''' 
+   Purpose: addition function used in both restoring and nonrestoring
+   Pre-Conditions: input two binary numbers
+   Post-Conditions: returns the sum of both binary numbers
+'''
 def add(num1,num2):
     i = len(num1) -1
 
@@ -230,33 +290,33 @@ def add(num1,num2):
     result = result[::-1]
     return result
 
-nonRestAddSubCount = 0 
-dividend = "010100011" # has to be EAQ 
-B = "01011"
-#metricsTable.field_names = ["Length of Dividend", "Length of Divisor" ,"Iterations", "Non-Restoring Add/Sub Count" , "Restoring Add/Sub Count"]
 
+'''
+   +---------------------------------------------------+
+   |                                                   |
+   |             Print Steps of Operation              |
+   |                                                   |
+   +---------------------------------------------------+
+'''
 
-print("\n\n\n")
+restAddSubCount = restoring(dividend, B) #logs all of the Restoring Steps
+nonRestAddSubCount = nonrestoring(dividend,B) #logs all of the nonRestoring Steps 
+metricsTable.add_row([len(dividend), len(B), len(B)-1, nonRestAddSubCount, restAddSubCount]) #logs the counts of each algorithm
 
-restAddSubCount = restoring(dividend, B)
+print("\n")
 print("+---------------------------------------------+")
 print("|                                             |")
 print("|             Restoring Algorithm             |")
 print("|                                             |")
 print("+---------------------------------------------+")
-
 print(restoringTable)
+print("\n")
 
-print("\n\n")
-
-nonRestAddSubCount = nonrestoring(dividend,B)
 print("+-------------------------------------------+")
 print("|                                           |")
 print("|           Non-Restoring Algorithm         |")
 print("|                                           |")
 print("+-------------------------------------------+")
 print(nonrestoringTable)
-
-print()
-metricsTable.add_row([len(dividend), len(B), len(B)-1, nonRestAddSubCount, restAddSubCount])
+print("\n")
 print(metricsTable)
